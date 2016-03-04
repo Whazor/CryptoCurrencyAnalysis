97f65dd0d671a3cac3f5df2078f1dd61aeeadb63
Nanne Wielinga <--global>
2016-03-04 15:04:32 +0100

    enhance download file

--- Downloader/src/Download.java
+++ Downloader/src/Download.java
@@ -1,8 +1,5 @@
 import com.google.common.util.concurrent.Service;
-import org.bitcoinj.core.Block;
-import org.bitcoinj.core.BlockChain;
-import org.bitcoinj.core.NetworkParameters;
-import org.bitcoinj.core.Peer;
+import org.bitcoinj.core.*;
 import org.bitcoinj.kits.WalletAppKit;
 import org.bitcoinj.params.MainNetParams;
 import org.bitcoinj.store.BlockStore;
@@ -11,13 +8,15 @@ import org.bitcoinj.utils.BriefLogFormatter;
 
 import java.sql.*;
 
+import java.util.List;
+import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
 /**
  * Created by nanne on 26/02/16.
  */
 public class Download {
-    public static void main(String[] args) throws BlockStoreException, ExecutionException, InterruptedException {
+    public static void main(String[] args) throws BlockStoreException, ExecutionException, InterruptedException, ClassNotFoundException, SQLException {
 
         BriefLogFormatter.init();
 
@@ -34,15 +33,31 @@ public class Download {
         Peer peer = kit.peerGroup().getDownloadPeer();
         Block b = peer.getBlock(bs.getChainHead().getHeader().getHash()).get();
 
+        Class.forName("org.postgresql.Driver");
+
+        String url = "jdbc:postgresql://localhost/bitcoin"; // user=fred&password=secret&ssl=true
+        Connection conn = DriverManager.getConnection(url);
 
 
-        b.getTime();
+        PreparedStatement statement = conn.prepareStatement("INSERT INTO Block (hashMerkleRoot, txn_counter) VALUES (?, ?)");
 
+        addBlock(b, statement);
         for (int i = 0; i < 1000; i++) {
-            b = peer.getBlock(b.getPrevBlockHash()).get(0).getTransactions().get(0).getExchangeRate();
-
-//            b.getTransactions().get(0)
-            System.out.println(b.getHashAsString());
+            addBlock(b, statement);
         }
+        statement.executeBatch();
+    }
+
+    private static void addBlock(Block b, PreparedStatement statement) throws SQLException {
+        statement.setString(1, b.getHashAsString());
+        int size = getSize(b);
+        statement.setInt(2, size);
+        statement.execute();
+        statement.addBatch();
+    }
+
+    private static int getSize(Block b) {
+        List<Transaction> list = b.getTransactions();
+        return list == null ? 0 : list.size();
     }
 }
