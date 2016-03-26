caaa7e40a3568aba8ee77c71ed241eaec831be31
Nanne Wielinga <--global>
2016-03-26 19:25:24 +0100

    add time to edge

--- Analyzer/src/Main.java
+++ Analyzer/src/Main.java
@@ -1,25 +1,19 @@
 import org.apache.flink.api.common.ExecutionConfig;
-import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.MapFunction;
-import org.apache.flink.api.common.functions.RichFilterFunction;
 import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
 import org.apache.flink.api.java.DataSet;
-import org.apache.flink.api.java.operators.DataSink;
-import org.apache.flink.api.java.operators.MapOperator;
-import org.apache.flink.graph.*;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.io.jdbc.JDBCInputFormat;
+import org.apache.flink.api.java.operators.MapOperator;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.api.java.typeutils.TupleTypeInfo;
-import org.apache.flink.types.NullValue;
+import org.apache.flink.graph.Edge;
 import org.apache.flink.graph.Graph;
+import org.apache.flink.graph.Vertex;
+import org.apache.flink.types.NullValue;
 import org.apache.flink.util.Collector;
-
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 
 /**
  * Created by s158079 on 11/03/2016.
@@ -34,24 +28,27 @@ public class Main {
         Class.forName("org.postgresql.Driver");
         // Read data from a relational database using the JDBC input format
 
-        DataSet<Tuple2<String, String>> data =
+        DataSet<Tuple3<String, String, Integer>> data =
                 env.createInput(
                         JDBCInputFormat.buildJDBCInputFormat()
                                 .setDrivername("org.postgresql.Driver")
                                 .setDBUrl("jdbc:postgresql://localhost/bitcoin")
                                 .setUsername("bitcoin")
                                 .setPassword("a")
-                                .setQuery("select \"TxnOut\".address, \"TxnIn\".address\n" +
+                                .setQuery("select \"TxnOut\".address, \"TxnIn\".address, extract(epoch from \"BlockHeader\".\"nTime\")\n" +
                                         "from \"TxnIn\"\n" +
-                                        "iNNER JOIN \"TxnOut\" USING (txn_id)")
+                                        "INNER JOIN \"TxnOut\" USING (txn_id)\n" +
+                                        "INNER JOIN \"Txn\" ON txn_id = \"Txn\".id\n" +
+                                        "INNER JOIN \"BlockHeader\" ON \"Txn\".block_id = \"BlockHeader\".id")
                                 .finish(),
-                        new TupleTypeInfo(Tuple2.class, BasicTypeInfo.STRING_TYPE_INFO, BasicTypeInfo.STRING_TYPE_INFO)
+                        new TupleTypeInfo(Tuple2.class, BasicTypeInfo.STRING_TYPE_INFO, BasicTypeInfo.STRING_TYPE_INFO, BasicTypeInfo.INT_TYPE_INFO)
                 );
 
 
-        DataSet<Vertex<String, NullValue>> vertices = data.flatMap(new FlatMapFunction<Tuple2<String, String>, String>() {
+
+        DataSet<Vertex<String, NullValue>> vertices = data.flatMap(new FlatMapFunction<Tuple3<String, String, Integer>, String>() {
             @Override
-            public void flatMap(Tuple2<String, String> tup, Collector<String> collector) throws Exception {
+            public void flatMap(Tuple3<String, String, Integer> tup, Collector<String> collector) throws Exception {
                 collector.collect(tup.f0);
                 collector.collect(tup.f1);
             }
@@ -60,21 +57,22 @@ public class Main {
             public Vertex<String, NullValue> map(String s) throws Exception {
                 return new Vertex<>(s, NullValue.getInstance());
             }
-        })
+        });
 
-        DataSet<Edge<String, NullValue>> edges = data.map(new MapFunction<Tuple2<String, String>, Edge<String, NullValue>>() {
+        DataSet<Edge<String, Integer>> edges = data.map(new MapFunction<Tuple3<String, String, Integer>, Edge<String, Integer>>() {
             @Override
-            public Edge<String, NullValue> map(Tuple2<String, String> tup) throws Exception {
-                return new Edge<>(tup.f0, tup.f1, NullValue.getInstance());
+            public Edge<String, Integer> map(Tuple3<String, String, Integer> tup) throws Exception {
+                return new Edge<>(tup.f0, tup.f1, tup.f2);
             }
         });
 
-        Graph<String, NullValue, NullValue> graph = Graph.fromDataSet(vertices, edges, env);
+        Graph<String, NullValue, Integer> set = Graph.fromDataSet(vertices, edges, env);
 
-        MapOperator<Edge<String, NullValue>, String> map = edges.map(new MapFunction<Edge<String, NullValue>, String>() {
+
+        MapOperator<Edge<String, Integer>, String> map = edges.map(new MapFunction<Edge<String, Integer>, String>() {
             @Override
-            public String map(Edge<String, NullValue> ed) throws Exception {
-                return ed.getSource() + " -> " + ed.getTarget();
+            public String map(Edge<String, Integer> ed) throws Exception {
+                return ed.getSource().hashCode() + " -> " + ed.getTarget().hashCode();
             }
         });
         map.writeAsText("file:///Users/nanne/Projects/School/CryptoCurrencyAnalysis/out/result.dot");
